ef07f160b2015e000574c7c6ed8d119702e1ab8b
abidoye korede <korexcoded@gmail.com>
2024-03-19 22:56:53 +0100

    updated files

---
+++ script.js
@@ -0,0 +1,26 @@
+// Function to add leading zero to single-digit numbers
+
+function formatNumber(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+// Set the target date for the countdown
+const targetDate = new Date('2024-03-27T23:59:59');
+
+setInterval(function() {
+  const currentDate = new Date();
+  const remainingTime = targetDate - currentDate;
+
+  // Calculate the remaining days, hours, minutes, and seconds
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+  // Update the countdown elements with the remaining time
+  document.querySelector('.firstNum').innerHTML = formatNumber(days);
+  document.querySelector('.secondNum').innerHTML = formatNumber(hours);
+  document.querySelector('.thirdNum').innerHTML = formatNumber(minutes);
+  document.querySelector('.fourthNum').innerHTML = formatNumber(seconds);
+
+}, 1000);
